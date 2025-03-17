f5160524610b7d472ad33680f5123198253700e3
Ghaith Al Saifi <114835018+galsaifi@users.noreply.github.com>
2025-03-17 16:15:36 -0500

    Add files via upload

---
+++ Business Certification/Veteran Certifications/Not Disadvantaged Veterans/Veteran Certfied.R
@@ -0,0 +1,175 @@
+library(stringdist)
+library(readxl)
+library(dplyr)
+library(data.table)
+library(writexl)
+
+
+dsbs <- read_excel("DSBS Veteran Owned.xlsx")
+neoserra <- read_excel("Neoserra Clients.xlsx")
+
+# Assigning APEX Consultants
+desired_consultants <- c("Charles (Chuck) Beck", "Harold Sargus", "Patrick Guinotte", "Meghann Buresh", "Carlos Garzon",
+                         "Quentin Farley")
+
+neoserra <- neoserra %>%
+  mutate(
+    `Consultant Area` = case_when(
+      (`Physical Address County` %in% c("DOUGLAS", "DODGE", "SAUNDERS", "WASHINGTON", "SARPY"))
+      ~ "Omaha", 
+      (`Physical Address County` %in% c("POLK", "PLATTE", "COLFAX", "BUTLER", "BOYD", "HOLT", "GARFIELD", "WHEELER",
+                                        "VALLEY", "GREELEY", "KNOX", "CEDAR", "DIXON", "ANTELOPE", "PIERCE", "WAYNE",
+                                        "DAKOTA", "THURSTON", "MADISON", "STANTON", "CUMING", "BURT", "BOONE", 
+                                        "NANCE", "MERRICK", "BOONE1"))
+      ~ "Norfolk", 
+      (`Physical Address County` %in% c("YORK", "SEWARD", "CASS", "CLAY", "FILLMORE", "SALINE", "NUCKOLLS", 
+                                        "THAYER", "GAGE", "JEFFERSON", "JOHNSON", "PAWNEE", "NEMAHA", "RICHARDSON", 
+                                        "LANCASTER", "OTOE"))
+      ~ "Lincoln",
+      TRUE ~ "Kearney"
+    ),
+    `Primary Consultants` = case_when(
+      `Primary Consultants` %in% desired_consultants ~ `Primary Consultants`,
+      `Consultant Area` == "Omaha" ~ "Omaha",
+      `Consultant Area` == "Norfolk" ~ "Meghann Buresh",
+      `Consultant Area` == "Lincoln" ~ "Quentin Farley",
+      `Consultant Area` == "Kearney" ~ "Charles (Chuck) Beck",
+      TRUE ~ `Primary Consultants` 
+    )
+  )
+
+non_id_clients <- neoserra %>%
+  filter(is.na(`Unique Entity Identifier`))
+
+preprocess_name <- function(name) {
+  name <- tolower(name)
+  name <- gsub("[[:punct:]]", "", name) 
+  name <- gsub("[[:space:]]+", " ", name) 
+  trimws(name)
+}
+
+neoserra$`Company Name` <- sapply(neoserra$`Company Name`, preprocess_name)
+dsbs$`Name of Firm` <- sapply(dsbs$`Name of Firm`, preprocess_name)
+
+
+# Matching clients without a UEI
+non_id_clients <- neoserra %>%
+  filter(is.na(`Unique Entity Identifier`))
+
+matches <- data.frame(Client_ID = character(),
+                      `Company Name` = character(),
+                      `Unique Entity Identifier` = character(),
+                      `Company Veteran Status` = character(),
+                      `Veteran Certification Date` = character(),
+                      `Veteran-Owned Cert Number` = character(),
+                      `Veteran-Owned Cert Date` = character(),
+                      `Veteran-Owned Expiration Date` = character(),
+                      `Primary Consultants` = character(),
+                      dsbs_name = character(),
+                      stringsAsFactors = FALSE)
+
+
+threshold <- 0.15
+
+for (i in 1:nrow(non_id_clients)) {
+  name1 <- non_id_clients$`Company Name`[i]
+  min_distance <- Inf
+  matched_name <- NA
+  matched_exit_date <- NA   # Initialize the exit date for each client in non_id_clients
+  
+  # Loop through dsbs to find a match
+  for (j in 1:nrow(dsbs)) {
+    name2 <- dsbs$`Name of Firm`[j]
+    distance <- stringdist::stringdistmatrix(name1, name2, method = "jw")
+    
+    if (distance < min_distance) {
+      min_distance <- distance
+      matched_name <- name2
+      vet_number <- dsbs$`Veteran-Owned Cert Number`[j]
+      vet_date <- dsbs$`Veteran-Owned Cert Date`[j]
+      vet_exp <- dsbs$`Veteran-Owned Expiration Date`[j]
+    }
+  }
+  
+  # If a match is found, add it to the matches dataframe
+  if (min_distance <= threshold) {
+    matches <- rbind(matches, data.frame(Client_ID = non_id_clients$`Client ID`[i],
+                                         `Company Name` = name1,
+                                         `Unique Entity Identifier` = non_id_clients$`Unique Entity Identifier`[i],
+                                         `Company Veteran Status` = non_id_clients$`Company Veteran Status`[i],
+                                         `Veteran Certification Date` = non_id_clients$`Veteran Certification Date`[i],
+                                         `Veteran-Owned Cert Number` = vet_number,
+                                         `Veteran-Owned Cert Date` = vet_date,
+                                         `Veteran-Owned Cert Expiration Date` = vet_exp,
+                                         `Primary Consultants` = non_id_clients$`Primary Consultants`[i],
+                                         dsbs_name = matched_name)) 
+  }
+}
+
+write_xlsx(matches, "Matches.xlsx") # export the matches data set and clean up manually
+
+filtered_matches <- read_excel("Matches.xlsx")
+
+colnames(filtered_matches) <- gsub("\\.", " ", colnames(matches))
+colnames(filtered_matches)[colnames(filtered_matches) == "Client_ID"] <- "Client ID"
+
+# Matching clients with UEIs
+uei_clients <- neoserra %>%
+  filter(!is.na(`Unique Entity Identifier`))
+
+joined <- left_join(uei_clients, dsbs, join_by(`Unique Entity Identifier` == UEI), multiple = "all")
+
+disadvantaged <- joined %>%
+  filter(!is.na(`Name of Firm`)) %>%
+  select(`Client ID`, `Company Name`, `Unique Entity Identifier`, `Company Veteran Status`, `Veteran Certification Date`,
+         `Veteran-Owned Cert Number`, `Veteran-Owned Cert Date`, `Veteran-Owned Expiration Date`,
+         `Primary Consultants`)
+
+# Combine the two
+combined_certify <- bind_rows(disadvantaged, filtered_matches)
+
+# Export the final data set
+write_xlsx(disadvantaged, "Veteran Owned Certified.xlsx")
+
+
+# Find certified clients that shouldn't be certified 
+# Clients that have a UEI
+dont_certify <- uei_clients %>%
+  filter(`Company Veteran Status` == "VOSB Certified")
+neoserra_not_in_dsbs <- anti_join(dont_certify, dsbs, join_by(`Unique Entity Identifier` == UEI))
+
+# Clients that do not have a UEI
+non_id_clients_certified <- non_id_clients %>%
+  filter(`Company Veteran Status` == c("VOSB Certified"))
+
+no_match_id <- data.frame()
+
+for (i in 1:nrow(non_id_clients_certified)) {
+  name1 <- non_id_clients_certified$`Company Name`[i]
+  has_match <- FALSE
+  
+  for (name2 in dsbs$`Name of Firm`) {
+    distance <- stringdist::stringdistmatrix(name1, name2, method = "jw")
+    
+    if (distance <= threshold) {
+      has_match <- TRUE
+      break
+    }
+  }
+  
+  if (!has_match) {
+    no_match_id <- rbind(no_match_id, non_id_clients_certified[i, ])
+  }
+}
+
+non_matches <- bind_rows(no_match_id, neoserra_not_in_dsbs)
+
+write_xlsx(non_matches, "Not Veteran Owned.xlsx")
+
+
+      
+      
+      
+
+
+
